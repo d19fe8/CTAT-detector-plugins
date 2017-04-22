779e380b0b10d6bfa7fac41436e836d19a76e1b2
Ken Holstein <d19fe8@gmail.com>
2017-04-22 00:13:58 -0400

    clean up of test files

--- HTML/pause/addActiveStudent.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$f = fopen("activeStudents.txt","a+");
-	$std = "hey".$_GET["student"];
-	fwrite($f, $std);
-	fclose($f);
-?>
--- HTML/pause/check.php
+++
@@ -1,6 +0,0 @@
-<?php
-$f = fopen("control.txt","r");
-$pauseplay = fread($f, filesize("control.txt"));
-echo $pauseplay;
-fclose($f);
-?>
